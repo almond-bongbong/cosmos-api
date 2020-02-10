225e6eacada8dfeeaf2c67224cf4af8ee295ef1b
cmlee <bal.dongdongdong@gmail.com>
2020-02-11 00:27:49 +0900

    initialize

---
+++ src/main/java/dev/cmlee/cosmosapi/CosmosApiApplication.java
@@ -0,0 +1,13 @@
+package dev.cmlee.cosmosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CosmosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/cmlee/cosmosapi/CosmosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.cmlee.cosmosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CosmosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
