74f0c80da6072aa61c139ebf4879d106663cf359
cmlee <bal.dongdongdong@gmail.com>
2020-02-11 23:17:36 +0900

    add swagger

---
+++ src/main/java/dev/cmlee/cosmosapi/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package dev.cmlee.cosmosapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	private static final String[] AUTH_WHITELIST = {
+			"/v3/api-docs",
+	};
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http
+				.cors()
+				.and()
+				.authorizeRequests()
+					.antMatchers(AUTH_WHITELIST).permitAll();
+	}
+}
