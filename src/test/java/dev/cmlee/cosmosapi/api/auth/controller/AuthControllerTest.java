2813584b660acce838f20d67a8fb707fc8735d6a
cmlee <bal.dongdongdong@gmail.com>
2020-02-16 19:41:06 +0900

    add kakao fail test

--- src/test/java/dev/cmlee/cosmosapi/api/auth/controller/AuthControllerTest.java
+++ src/test/java/dev/cmlee/cosmosapi/api/auth/controller/AuthControllerTest.java
@@ -5,30 +5,32 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@ActiveProfiles("test")
 class AuthControllerTest {
 
 	@Autowired
 	MockMvc mockMvc;
 
-//	@Test
-//	public void authKakaoFail() throws Exception {
-//		String body = new JSONObject()
-//				.put("accessToken", "invalidToken")
-//				.toString();
-//
-//		mockMvc.perform(post("/auth/kakao")
-//					.content(body))
-//				.andDo(print())
-//				.andExpect(status().isUnauthorized());
-//	}
+	@Test
+	public void authKakaoFail() throws Exception {
+		String body = new JSONObject()
+				.put("accessToken", "invalidToken")
+				.toString();
+
+		mockMvc.perform(post("/auth/kakao")
+					.contentType(MediaType.APPLICATION_JSON)
+					.content(body))
+				.andDo(print())
+				.andExpect(status().isUnauthorized())
+				.andExpect(jsonPath("message").exists());
+	}
 }
